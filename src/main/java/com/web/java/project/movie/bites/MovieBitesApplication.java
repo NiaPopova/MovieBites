9e5b3f2fab098be3972e3d53b433aaac6cd0e0e9
Nia <fn45622@g.fmi.uni-sofia.bg>
2025-05-08 13:05:11 +0300

    initial project

---
+++ src/main/java/com/web/java/project/movie/bites/MovieBitesApplication.java
@@ -0,0 +1,13 @@
+package com.web.java.project.movie.bites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBitesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/java/project/movie/bites/MovieBitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.java.project.movie.bites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
