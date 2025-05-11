3febb1a19981dc9fbcf4a3d4a70dc04b2b3c7df0
Nia <fn45622@g.fmi.uni-sofia.bg>
2025-05-11 19:03:10 +0300

    Added entities for Movie and Genre

---
+++ src/main/java/com/web/java/project/movie/bites/entities/Movie.java
@@ -0,0 +1,39 @@
+package com.web.java.project.movie.bites.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Movies")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @Column(name = "YEAR")
+    private Integer year;
+
+    @Column(name = "DIRECTOR")
+    private String director;
+
+    @Column(name = "SUMMARY")
+    private String summary;
+
+    @Column(name = "PHOTO")
+    private String photo;
+
+    @ManyToOne
+    @JoinColumn(name = "GENRE_ID")
+    private Genre genre;
+}
