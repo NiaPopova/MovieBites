3071ded63453178f33e17d4f4c1eb1859dbf03c0
Nia <fn45622@g.fmi.uni-sofia.bg>
2025-06-30 16:52:49 +0300

    fixed imports

--- src/main/java/com/web/java/project/movie/bites/controllers/UserController.java
+++ src/main/java/com/web/java/project/movie/bites/controllers/UserController.java
@@ -8,10 +8,16 @@ import com.web.java.project.movie.bites.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.*;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
