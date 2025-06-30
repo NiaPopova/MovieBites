774b6d29ac2eeeedf9a2c2b1ec3ff228567f7ee8
Nia <fn45622@g.fmi.uni-sofia.bg>
2025-06-30 16:51:40 +0300

    created exception handler

---
+++ src/main/java/com/web/java/project/movie/bites/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.web.java.project.movie.bites.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
